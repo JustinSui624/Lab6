befa034d6d3814df672f9387ec5a0b30697e863d
JustinSui624 <139939511+JustinSui624@users.noreply.github.com>
2023-07-19 21:53:12 -0400

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -9,31 +9,25 @@ def menu():
     
     
 def encode(user_password):
-    count = 0
-    encoded_pass = ''
-    while count < len(user_password):
-        if int(user_password[count]) > 7:
-            encoded_pass += str(int(user_password[count]) + 3)
-            count += 1
-        elif int(user_password[count]) == 7:
-            encoded_pass += "0"
-        elif int(user_password[count]) == 8:
-            encoded_pass += "1"
-        elif int(user_password[count]) == 9:
-            encoded_pass += "2"
-    return encoded_pass
-
+    password = []
+    for i in iter(str(user_password)):
+        s = int(i)+3
+        password.append(s)
+        
+    return password
     
 while True:
     menu()
     option = int(input("Please enter an option: "))
     if option ==1:
-        user_password = int(input("Please enter your password to encode:"))
+        user_password = int(input("Please enter your password to encode: "))
         password = encode(user_password)        
         print('Your password has been encoded and stored!')
     elif option==2:
         decoded_password = decode(password)
-        print("The encoded password is", password, "and the original password is", decoded_password + ".")
+        password1 = ''.join(str(x) for x in password)
+        print("The encoded password is", password1, "and the original password is", decoded_password + ".")
     elif option==3:
         break
+        
 
